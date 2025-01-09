489c499e11855b7ffb65a2a9475042a92409c82c
Yen Tran <yentran@Yens-MBP.lan>
2025-01-08 21:04:11 -1000

    change dice  from number to object

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Die from './Die';
+import { nanoid } from 'nanoid';
 
 export default function App() {
   const [dice, setDice] = useState(generateAllNewDice());
@@ -7,10 +8,14 @@ export default function App() {
   function generateAllNewDice() {
     return new Array(10)
       .fill(0)
-      .map(() => Math.ceil(Math.random() * 6))
+      .map(() => ({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+      }))
   }
 
-  const dieElements = dice.map((num, index) => <Die key={index} value={num}/>)
+  const dieElements = dice.map(die => <Die key={die.id} value={die.value} />)
 
   function handleClick() {
     setDice(generateAllNewDice())
