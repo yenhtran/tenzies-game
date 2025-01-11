b41f7a3dcffa3187646f8cf695eb3b1279da59ec
Yen Tran <yentran@Yens-MBP.lan>
2025-01-10 18:00:19 -1000

    add game description and title

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,8 @@ export default function App() {
 
   return (
     <main>
+      <h1 className='title'>Tenzies</h1>
+      <p className='instructions'>Roll until all die are the same. Click each die to freeze it at its current value between rolls</p>
       <div className='game-board'>
         {dieElements}
       </div>
